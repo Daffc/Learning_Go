9d9bb8a6f3dc37f6fad5a989faf4f0755758dfbd
Douglas A.C <douglas.affc@gmail.com>
2023-07-21 21:03:38 -0300

    Select

---
+++ Select/Select.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(1 * time.Second)
+		c1 <- "one"
+	}()
+	go func() {
+		time.Sleep(2 * time.Second)
+		c2 <- "two"
+	}()
+
+	// Loops in order to wait for all go routines.
+	for i := 0; i < 2; i++ {
+
+		// Awaits util one goroutines finishes (one channels have a value to be received).
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
+}
